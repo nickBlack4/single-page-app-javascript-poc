b9dcbc2d27ce9f72c5dc156b1e803a646bda12dc
nicholascblack4@gmail.com <nicholascblack4@gmail.com>
2018-07-19 11:35:06 -0500

    a lot of updates storing data to local storage, pulling it from there and starting to populate

--- data.js
+++ data.js
@@ -1,14 +1,59 @@
+/*
+	objects/info to be stored in local storage and retrieved
+*/
 
 
-let executive = {
+// let executive = {
 
+// 	"collaborators": ["mario", "luigi"],
+// 	"portrait": {
+// 		"name": "Nicholas Cage",
+// 		"photo_url": "blah"// attach urls here and populate slider?
+// 	},
+// 	"aliases": ["Nic", "Nick", "Nicholas", "Bossman"],
+// 	"residences": ["USA", "Britain", "France"]
+// }
 
-	"collaborators": ["mario", "luigi"],
-	"portrait": {
-		"name": "Nicholas Cage",
-		"photo_url": "blah"// attach urls here and populate slider?
-	},
-	"aliases": ["Nic", "Nick", "Nicholas", "Bossman"],
-	"residences": ["USA", "Britain", "France"]
+/*
+*	note the structure of the database here.
+*  it is an object, with arrays that contain objects.
+*  I believe in doing this, steve preserved the ability to use
+* methods native to array prototype
+*/
+
+const localStorageKey = "nickStorage";
+
+// initialize the database in memory as an object 
+const HomeInventoryDatabase = {};
+
+// Init all of the tables (i.e. arrays) that you want to use in DB
+
+HomeInventoryDatabase.furniture = [];
+HomeInventoryDatabase.crafts = [];
+HomeInventoryDatabase.electronics = [];
+
+// Create data that will be inserted into objects
+
+const vintageInkwell = {
+	
+	name: "Vintage Ink Well",
+	location: "Writing desk",
+	description: "I enjoy this inkwell because it belonged to pop"
 }
+
+const writingDesk = {
+	
+	
+	name: "Shaker writing desk",
+	location: "Bedroom",
+	description: "This antique desk blah blah"
+}
+
+// Add the data to the appropriate tables
+HomeInventoryDatabase.crafts.push(vintageInkwell);
+HomeInventoryDatabase.furniture.push(writingDesk);
+
+// Persist the database to localStorage
+saveDatabase(HomeInventoryDatabase, "HomeInventory");
+
 
