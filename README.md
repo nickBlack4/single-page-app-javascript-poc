4c26f85e0b724c3cbc8cf75a91c9b63890400018
Nicholas Black <nicholas.c.black@leidos.com>
2019-11-01 10:31:28 -0400

    updated the Readme and a string in array just to verify changes

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # tributeProjectSandbox
 
-### About
+## About
+
+### Updated about 11/1/19
+I was running through repos cleaning things up when I saw this.  I used this as an example to convince some team members when I was at NSS that we could load a single page 'app' this way.  We were working on a collaborative project and rather than us all manipulating multiple html files and trying to string them together with different css, etc. I thought we should just have a container on the main page.  In this container we could load each other's data based on the navigation link you clicked.  We ended up doing it this way and I saved this for reference in the future.
+
+---
 
 this is a sandbox for me to test pulling data from a .js file and 
-get it into local storage and out of local storage, and populate it withini a main container on the DOM 
+get it into local storage and out of local storage, and populate it within a main container on the DOM 
 
 - [x] encode data into json objects
 - [x] store data to local storage
--- data.js
+++ data.js
@@ -2,10 +2,9 @@
 	objects/info to be stored in local storage and retrieved
 */
 
-
 // let executive = {
 
-// 	"collaborators": ["mario", "luigi"],
+// 	"collaborators": ["mario", "luigi", "sam"],
 // 	"portrait": {
 // 		"name": "Nicholas Cage",
 // 		"photo_url": "blah"// attach urls here and populate slider?
@@ -15,20 +14,25 @@
 // }
 
 /*
-*	note the structure of the database here.
-*  it is an object, with arrays that contain objects.
-*  I believe in doing this, steve preserved the ability to use
-* methods native to array prototype
-*/
+ *	note the structure of the database here.
+ *  it is an object, with arrays that contain objects.
+ *  I believe in doing this, steve preserved the ability to use
+ * methods native to array prototype
+ */
 
 const localStorageKey = "nickStorage";
 
-// initialize the database in memory as an object 
+// initialize the database in memory as an object
 const HomeInventoryDatabase = {};
 
 // Init all of the tables (i.e. arrays) that you want to use in DB
 
-HomeInventoryDatabase.collaborators = ["mario", "luigi", "donald ducks"];
+HomeInventoryDatabase.collaborators = [
+	"mario",
+	"luigi",
+	"donald duck",
+	"yosemite sam"
+];
 // filled (pushed) below with portraitInfo object
 HomeInventoryDatabase.portrait = [];
 HomeInventoryDatabase.aliases = ["Nic", "Nick", "Nicholas", "Bossman"];
@@ -42,27 +46,22 @@ HomeInventoryDatabase.residence = ["USA"];
 // we can populate any images we want to the dom as long as we have the url
 const portraitInfo = {
 	name: "Nicholas Cage",
-	photo: "photoURLhereToCreateImageObjectAndSrcFrom",
-}
+	photo: "photoURLhereToCreateImageObjectAndSrcFrom"
+};
 
 // Add the data to the appropriate tables
 HomeInventoryDatabase.portrait.push(portraitInfo);
 
-
-
 // I needed to put saveDatabase in here so it had access to localStorage variable
-const saveDatabase = function (databaseObject) {
-    // convert the object into a string.
-    const stringifiedDatabase = JSON.stringify(databaseObject);
-    
-    // create a key in local storage, and store the string version
-    // of your inventory database as the value
-  
-    localStorage.setItem(localStorageKey, stringifiedDatabase);
-}
+const saveDatabase = function(databaseObject) {
+	// convert the object into a string.
+	const stringifiedDatabase = JSON.stringify(databaseObject);
 
+	// create a key in local storage, and store the string version
+	// of your inventory database as the value
+
+	localStorage.setItem(localStorageKey, stringifiedDatabase);
+};
 
 // Persist the database to localStorage
 saveDatabase(HomeInventoryDatabase, "HomeInventory");
-
-
